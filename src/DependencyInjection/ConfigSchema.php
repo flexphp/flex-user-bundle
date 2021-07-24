fe6bdee759c11834a05e883be272ad52aef66482
Freddie Gar <freddie.gar@outlook.com>
2021-07-24 06:43:02 -0500

    Initial commit

---
+++ src/DependencyInjection/ConfigSchema.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of FlexPHP.
+ *
+ * (c) Freddie Gar <freddie.gar@outlook.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace FlexPHP\Bundle\UserBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+// @see https://symfony.com/doc/current/components/config/definition.html
+class ConfigSchema implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder('user-bundle');
+
+        $treeBuilder->getRootNode()
+            ->children()
+            ->end();
+
+        return $treeBuilder;
+    }
+}
---
+++ src/DependencyInjection/UserExtension.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of FlexPHP.
+ *
+ * (c) Freddie Gar <freddie.gar@outlook.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace FlexPHP\Bundle\UserBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+class UserExtension extends Extension
+{
+    public function getContainerExtension(): string
+    {
+        return 'flexphp_user';
+    }
+
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../../config'));
+        $loader->load('services.xml');
+
+        $schema = new ConfigSchema();
+
+        $this->processConfiguration($schema, $configs);
+    }
+}
